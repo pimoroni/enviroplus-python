998bb2e90a2bab0ce43f92ee0988bfab3a29ea96
Phil Howard <phil@gadgetoid.com>
2019-08-20 16:17:12 +0100

    Dropped obsolete .decode()

--- examples/all-in-one-no-pm.py
+++ examples/all-in-one-no-pm.py
@@ -80,7 +80,6 @@ def display_text(variable, data, unit):
 def get_cpu_temperature():
     process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
     output, _error = process.communicate()
-    output = output.decode()
     return float(output[output.index('=') + 1:output.rindex("'")])
 
 
--- examples/luftdaten.py
+++ examples/luftdaten.py
@@ -74,7 +74,6 @@ def read_values():
 def get_cpu_temperature():
     process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
     output, _error = process.communicate()
-    output = output.decode()
     return float(output[output.index('=') + 1:output.rindex("'")])
 
 
