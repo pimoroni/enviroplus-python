10a3bf592b543e31dd2bd32557ec1fb43b118019
Phil Howard <github@gadgetoid.com>
2026-09-18 15:46:42 +0100

    Adopt tag-derived versioning and PyPI trusted publishing

--- enviroplus/__init__.py
+++ enviroplus/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "1.0.2"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("enviroplus")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
